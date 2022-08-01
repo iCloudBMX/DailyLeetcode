3cf3c256fe253d37028304d16adf9765437966d2
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-08-01 06:32:46 +0500

    Add project files.

---
+++ DailyLeetcode.Solutions/Arrays/Two pointers/PalindromeWord.cs
@@ -0,0 +1,16 @@
+﻿namespace DailyLeetcode.Solutions
+{
+    public static class PalindromeWord
+    {
+        public static bool IsPalindrome(string word)
+        {
+            int left = 0, right = word.Length - 1;
+
+            while (left < right)
+                if (word[left--] != word[right--])
+                    return false;
+
+            return true;
+        }
+    }
+}
