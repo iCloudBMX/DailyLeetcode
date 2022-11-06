275c2b6946c52dc96099cf66119b7582d90ea128
iCloudBMX <iCloudBMX0525@gmail.com>
2022-11-06 08:31:51 +0500

    Implemented insertion sort

---
+++ DailyLeetcode.Solutions/Sorting/Algorithms/InsertionSort.cs
@@ -0,0 +1,26 @@
+﻿namespace DailyLeetcode.Solutions.Sorting.Algorithms;
+
+/**
+ * Time complexity - O(n^2)
+ * Space complexity - O(1)
+**/
+
+public static class InsertionSort
+{
+    public static void SortWithInsertionSort(this int[] array)
+    {
+        for(int i = 1; i < array.Length; i++)
+        {
+            int current = array[i];
+            int j = i - 1;
+
+            while(j >= 0 && array[j] > current)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = current;
+        }
+    }
+}
